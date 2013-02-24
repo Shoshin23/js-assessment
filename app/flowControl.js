292b227accb015b7df1571242509db071805998b
Karthik Kannan <karthikk.314@gmail.com>
2013-02-24 19:37:57 +0530

    completed the infamous fizz buzz test.

--- app/flowControl.js
+++ app/flowControl.js
@@ -3,14 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     fizzBuzz : function(num) {
-      // write a function that receives a number as its argument;
-      // if the number is divisible by 3, the function should return 'fizz';
-      // if the number is divisible by 5, the function should return 'buzz';
-      // if the number is divisible by 3 and 5, the function should return
-      // 'fizzbuzz';
-      //
-      // otherwise the function should return the number, or false if no number
-      // was provided or the value provided is not a number
+   if (typeof num != "number") { 
+	   return false; 
+   }
+// because if first or the second part of the condition fails then the entire expression fails returning num.
+      if (num % 3 && num % 5) {
+        return num;
+      }
+
+      if (num % 5) {
+        return 'fizz';
+      }
+
+      if (num % 3) {
+        return 'buzz';
+      }
+
+      return 'fizzbuzz';
     }
   };
 });
